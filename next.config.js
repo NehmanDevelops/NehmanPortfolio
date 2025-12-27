8a61185c86b1c2697c699554fa1982f3bfc53abc
Nehman <nehmanguy@gmail.com>
2025-12-26 21:15:51 -0500

    Add Vercel config and webpack optimizations to fix build issues

--- next.config.js
+++ next.config.js
@@ -15,6 +15,37 @@ const nextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+  webpack: (config, { isServer }) => {
+    // Increase memory limit for webpack
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          // Vendor chunk
+          vendor: {
+            name: 'vendor',
+            chunks: 'all',
+            test: /node_modules/,
+            priority: 20
+          },
+          // Common chunk
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            priority: 10,
+            reuseExistingChunk: true,
+            enforce: true
+          }
+        }
+      }
+    };
+    
+    return config;
+  },
 }
 
 module.exports = nextConfig
